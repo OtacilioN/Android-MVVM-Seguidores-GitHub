985ba537184d7adfc9720dc5a40a459da72e93de
OtacilioN <contato@otaciliomaia.com>
2021-04-27 22:50:52 -0300

    Increase per_page size

--- app/src/main/java/com/otaciliomaia/mvvm/data/api/ApiServiceImpl.kt
+++ app/src/main/java/com/otaciliomaia/mvvm/data/api/ApiServiceImpl.kt
@@ -6,7 +6,7 @@ import io.reactivex.Single
 
 class ApiServiceImpl : ApiService {
     override fun getUsers(): Single<List<User>> {
-        return Rx2AndroidNetworking.get("https://api.github.com/users/OtacilioN/followers")
+        return Rx2AndroidNetworking.get("https://api.github.com/users/OtacilioN/followers?page=1&per_page=1000")
             .build()
             .getObjectListSingle(User::class.java)
     }
